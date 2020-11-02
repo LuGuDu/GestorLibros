f40031657dbd3a3cd98deb9aa55bec3d5a067ce6
Lucas Gutierrez <70801680+LuGuDu@users.noreply.github.com>
2020-11-02 14:28:33 +0100

    Subida de archivos
    Subida la primera version del programa, solo consola

---
+++ src/usuario.java
@@ -0,0 +1,19 @@
+public class usuario {
+	
+	String nick;
+	String password;
+	
+	public usuario (String nick, String password) {		
+		this.nick = nick;
+		this.password = password;
+	}
+
+	public String getNick() {
+		return nick;
+	}
+
+	public String getPassword() {
+		return password;
+	}	
+	
+}
